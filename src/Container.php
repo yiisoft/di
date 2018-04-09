6cee31b359874aadd071c9c072745588f48674a5
Andrii Vasyliev <sol@hiqdev.com>
2018-04-09 12:12:49 +0300

    Added new `Container` and `Factory`

---
+++ src/Container.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\di;
+
+use Psr\Container\ContainerInterface;
+
+/**
+ * Container implements a [dependency injection](http://en.wikipedia.org/wiki/Dependency_injection) container.
+ *
+ * @author Alexander Makarov <sam@rmcreative.ru>
+ * @since 1.0
+ */
+class Container extends AbstractContainer implements ContainerInterface
+{
+    /**
+     * @var object[]
+     */
+    private $instances;
+
+    public function set(string $id, $definition): void
+    {
+        $this->instances[$id] = null;
+
+        parent::set($id, $definition);
+    }
+
+    /**
+     * Returns an instance by either interface name or alias.
+     *
+     * Same instance of the class will be returned each time this method is called.
+     *
+     * @param string $id the interface name or an alias name (e.g. `foo`) that was previously registered via [[set()]].
+     * @return object an instance of the requested interface.
+     */
+    public function get($id)
+    {
+        $id = $this->dereference($id);
+
+        if (isset($this->instances[$id])) {
+            return $this->instances[$id];
+        }
+
+        $object = $this->build($id);
+        $this->instances[$id] = $object;
+
+        return $object;
+    }
+}
