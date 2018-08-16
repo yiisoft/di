58aa6c668a187d330e56a3418d125da6053ee6e8
Andrii Vasyliev <sol@hiqdev.com>
2018-08-16 08:45:45 +0000

    Added tests for `Factory::get(`

--- tests/FactoryTest.php
+++ tests/FactoryTest.php
@@ -11,20 +11,45 @@ use yii\di\tests\code\EngineMarkOne;
  */
 class FactoryTest extends TestCase
 {
-    public function testCreatesNewByAlias()
+    public function testCreateByAlias()
     {
         $factory = new Factory();
         $factory->set('engine', EngineMarkOne::class);
         $one = $factory->create('engine');
         $two = $factory->create('engine');
         $this->assertFalse($one === $two);
+        $this->assertInstanceOf(EngineMarkOne::class, $one);
+        $this->assertInstanceOf(EngineMarkOne::class, $two);
     }
 
-    public function testCreatesNewByClass()
+    public function testCreateByClass()
     {
         $factory = new Factory();
         $one = $factory->create(EngineMarkOne::class);
         $two = $factory->create(EngineMarkOne::class);
         $this->assertFalse($one === $two);
+        $this->assertInstanceOf(EngineMarkOne::class, $one);
+        $this->assertInstanceOf(EngineMarkOne::class, $two);
+    }
+
+    public function testGetByAlias()
+    {
+        $factory = new Factory();
+        $factory->set('engine', EngineMarkOne::class);
+        $one = $factory->get('engine');
+        $two = $factory->get('engine');
+        $this->assertFalse($one === $two);
+        $this->assertInstanceOf(EngineMarkOne::class, $one);
+        $this->assertInstanceOf(EngineMarkOne::class, $two);
+    }
+
+    public function testGetByClass()
+    {
+        $factory = new Factory();
+        $one = $factory->get(EngineMarkOne::class);
+        $two = $factory->get(EngineMarkOne::class);
+        $this->assertFalse($one === $two);
+        $this->assertInstanceOf(EngineMarkOne::class, $one);
+        $this->assertInstanceOf(EngineMarkOne::class, $two);
     }
 }
