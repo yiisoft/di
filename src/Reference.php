9c3f601e7bef1c31a5003ce7dba883442c4e490a
Andrii Vasyliev <sol@hiqdev.com>
2019-04-14 16:15:21 +0200

    Added `__set_state` to Reference

--- src/Reference.php
+++ src/Reference.php
@@ -1,10 +1,15 @@
 <?php
-
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
 
 namespace yii\di;
 
 use Psr\Container\ContainerInterface;
 use yii\di\contracts\DependencyInterface;
+use yii\di\exceptions\InvalidConfigException;
 
 /**
  * Class Reference allows us to define a dependency to a service in the container in another service definition.
@@ -46,5 +51,24 @@ class Reference implements DependencyInterface
     public function resolve(ContainerInterface $container)
     {
         return $container->get($this->id);
+    }
+
+    /**
+     * Restores class state after using `var_export()`.
+     *
+     * @param array $state
+     * @return self
+     * @throws InvalidConfigException when $state property does not contain `id` parameter
+     * @see var_export()
+     */
+    public static function __set_state($state)
+    {
+        if (!isset($state['id'])) {
+            throw new InvalidConfigException(
+                'Failed to instantiate class "Reference". Required parameter "id" is missing'
+            );
+        }
+
+        return new self($state['id']);
     }
 }
