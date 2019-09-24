17a6d3dc1ddbd2d1671a9fe36468726db7ba7200
Dmitriy Derepko <xepozz@list.ru>
2019-09-24 21:22:55 +0300

    Renamed variables

--- tests/Unit/CompositeContextContainerTest.php
+++ tests/Unit/CompositeContextContainerTest.php
@@ -24,7 +24,7 @@ class CompositeContextContainerTest extends TestCase
     {
         $composite = new CompositeContextContainer();
 
-        $simple1 = new Container([
+        $container1 = new Container([
             PropertyTestClass::class => [
                 '__class' => PropertyTestClass::class,
             ],
@@ -33,32 +33,32 @@ class CompositeContextContainerTest extends TestCase
             ],
         ], []);
 
-        $simple2 = new Container([
+        $container2 = new Container([
             PropertyTestClass::class => [
                 '__class' => PropertyTestClass::class,
             ],
         ], []);
 
-        $simple3 = new Container([
+        $container3 = new Container([
             PropertyTestClass::class => [
                 '__class' => PropertyTestClass::class,
             ],
             EngineInterface::class => EngineMarkOne::class
         ], []);
 
-        $composite->attach($simple1);
-        $composite->attach($simple2, '/a');
-        $composite->attach($simple3, '/a/b');
+        $composite->attach($container1);
+        $composite->attach($container2, '/a');
+        $composite->attach($container3, '/a/b');
 
-        $this->assertSame($simple1->get(PropertyTestClass::class), $composite->get(PropertyTestClass::class));
-        $this->assertNotSame($simple2->get(PropertyTestClass::class), $composite->get(PropertyTestClass::class));
-        $this->assertNotSame($simple3->get(PropertyTestClass::class), $composite->get(PropertyTestClass::class));
+        $this->assertSame($container1->get(PropertyTestClass::class), $composite->get(PropertyTestClass::class));
+        $this->assertNotSame($container2->get(PropertyTestClass::class), $composite->get(PropertyTestClass::class));
+        $this->assertNotSame($container3->get(PropertyTestClass::class), $composite->get(PropertyTestClass::class));
 
         $contextA = $composite->getContextContainer('/a');
         $this->assertTrue($contextA->has(Car::class));
         $this->assertFalse($contextA->has(EngineInterface::class));
-        $this->assertNotSame($simple1->get(PropertyTestClass::class), $contextA->get(PropertyTestClass::class));
-        $this->assertSame($simple2->get(PropertyTestClass::class), $contextA->get(PropertyTestClass::class));
+        $this->assertNotSame($container1->get(PropertyTestClass::class), $contextA->get(PropertyTestClass::class));
+        $this->assertSame($container2->get(PropertyTestClass::class), $contextA->get(PropertyTestClass::class));
 
         $contextAB = $composite->getContextContainer('/a/b');
         $this->assertTrue($contextAB->has(Car::class));
