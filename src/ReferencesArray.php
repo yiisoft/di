f24f3cbbe909f5b9cb03c96e3f9b887f4f22caff
yiiliveext <yiiliveext@gmail.com>
2021-08-12 00:33:03 +0300

    Add phpdoc

--- src/ReferencesArray.php
+++ src/ReferencesArray.php
@@ -10,6 +10,49 @@ use Yiisoft\Factory\Exception\InvalidConfigException;
 final class ReferencesArray
 {
     /**
+     *  A usage example
+     *
+     * ```php
+     * //params.php
+     * return [
+     *      'yiisoft/data-response' => [
+     *          'contentFormatters' => [
+     *              'text/html' => HtmlDataResponseFormatter::class,
+     *              'application/xml' => XmlDataResponseFormatter::class,
+     *              'application/json' => JsonDataResponseFormatter::class,
+     *          ],
+     *      ],
+     * ];
+     * ```
+     *
+     * This definition
+     *
+     * ```php
+     * //web.php
+     *
+     * ContentNegotiator::class => [
+     *     '__construct()' => [
+     *         'contentFormatters' => ReferencesArray::from($params['yiisoft/data-response']['contentFormatters']),
+     *     ],
+     * ],
+     * ```
+     *
+     * equals to
+     *
+     * ```php
+     * //web.php
+     *
+     * ContentNegotiator::class => [
+     *     '__construct()' => [
+     *         'contentFormatters' => [
+     *             'text/html' => Reference::to(HtmlDataResponseFormatter())),
+     *             'application/xml' => Reference::to(XmlDataResponseFormatter()),
+     *             'application/json' => Reference::to(JsonDataResponseFormatter()),
+     *         ],
+     *     ],
+     * ],
+     * ```
+     *
      * @param array $ids
      * @return array
      * @throws InvalidConfigException
