0145d9716618ea72b48bd31d392d6ea9dccbf393
Alexander Makarov <samdark@users.noreply.github.com>
2021-08-31 09:11:18 +0000

    Apply fixes from StyleCI

--- tests/Support/DefinitionStorage/ServiceWithNonExistingSubDependency.php
+++ tests/Support/DefinitionStorage/ServiceWithNonExistingSubDependency.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Yiisoft\Di\Tests\Support\DefinitionStorage;
 
-final class ServiceWithInvalidSubDependency
+final class ServiceWithNonExistingSubDependency
 {
     public function __construct(ServiceWithNonExistingDependency $serviceWithInvalidDependency)
     {
