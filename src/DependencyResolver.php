137a67d02fc71e88f800dd221988632498b97e35
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2021-09-06 23:05:09 +0300

    Adopt to new `yiisoft/defenitions`

--- src/DependencyResolver.php
+++ src/DependencyResolver.php
@@ -8,7 +8,6 @@ use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
 use Psr\Container\NotFoundExceptionInterface;
 use Yiisoft\Definitions\Contract\DependencyResolverInterface;
-use Yiisoft\Injector\Injector;
 
 /**
  * @internal
@@ -16,7 +15,6 @@ use Yiisoft\Injector\Injector;
 final class DependencyResolver implements DependencyResolverInterface
 {
     private ContainerInterface $container;
-    private ?Injector $injector = null;
 
     public function __construct(ContainerInterface $container)
     {
@@ -24,8 +22,6 @@ final class DependencyResolver implements DependencyResolverInterface
     }
 
     /**
-     * @param string $id
-     *
      * @throws NotFoundExceptionInterface
      * @throws ContainerExceptionInterface
      *
@@ -33,28 +29,13 @@ final class DependencyResolver implements DependencyResolverInterface
      *
      * @psalm-suppress InvalidThrow
      */
-    public function get($id)
+    public function resolve(string $id)
     {
         return $this->container->get($id);
     }
 
-    public function has($id): bool
-    {
-        return $this->container->has($id);
-    }
-
     public function resolveReference(string $id)
     {
-        return $this->get($id);
-    }
-
-    public function invoke(callable $callable)
-    {
-        return $this->getInjector()->invoke($callable);
-    }
-
-    private function getInjector(): Injector
-    {
-        return $this->injector ??= new Injector($this->container);
+        return $this->resolve($id);
     }
 }
