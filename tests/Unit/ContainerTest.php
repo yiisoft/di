33e9b4c4450b3435ac9fb0f9fda477ce4586cdae
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-03-19 22:59:44 +0300

    Add test on exception in delegate

--- tests/Unit/ContainerTest.php
+++ tests/Unit/ContainerTest.php
@@ -2016,4 +2016,31 @@ final class ContainerTest extends TestCase
             $exception->getPrevious()
         );
     }
+
+    public function testExceptionInDelegate(): void
+    {
+        $container = new Container(
+            ContainerConfig::create()->withDelegates([
+                static fn () => new SimpleContainer(
+                    factory: static fn () => throw new RuntimeException('Error in delegate'),
+                ),
+            ]),
+        );
+
+        $exception = null;
+        try {
+            $container->get('identifier');
+        } catch (Throwable $exception) {
+        }
+
+        $this->assertInstanceOf(BuildingException::class, $exception);
+        $this->assertSame(
+            'Caught unhandled error "Error in delegate" while building "identifier".',
+            $exception->getMessage(),
+        );
+
+        $previous = $exception->getPrevious();
+        $this->assertInstanceOf(RuntimeException::class, $previous);
+        $this->assertSame('Error in delegate', $previous->getMessage());
+    }
 }
