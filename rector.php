23c57fdac6c19e77eb3bf6507f603abfaa805ccc
Sergei Predvoditelev <sergei@predvoditelev.ru>
2025-05-30 14:33:29 +0300

    Bump dev deps + Update rector configuration

--- rector.php
+++ rector.php
@@ -6,26 +6,19 @@ use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
 use Rector\Php81\Rector\Array_\FirstClassCallableRector;
-use Rector\Set\ValueObject\LevelSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
-    ]);
-
-    // register a single rule
-    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-
-    // define sets of rules
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
-    ]);
-
-    $rectorConfig->skip([
+    ])
+    ->withPhpSets(php81: true)
+    ->withRules([
+        InlineConstructorDefaultToPropertyRector::class,
+    ])
+    ->withSkip([
         ClosureToArrowFunctionRector::class,
         FirstClassCallableRector::class => [
             __DIR__ . '/tests/Unit/Helpers/DefinitionParserTest.php',
         ],
     ]);
-};
